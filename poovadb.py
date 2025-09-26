8de17e69c9aad3e6dd958f83c2013e907526693d
Lakmal <drlwelgama@gmail.com>
2025-09-26 09:44:19 +0530

    sep26

--- poovadb.py
+++ poovadb.py
@@ -15,36 +15,49 @@ db = firestore.Client(credentials=credentials)
 
 def update_document():
 
-    imeis = [866228071946701,
-    866228071945026,
-    866228071903389,
-    866228071941926,
-    866228071946701,
-    866228071945026,
-    866228071903389,
-    866228071942148,
-    866228071902209,
-    866228071941926,
-    866228071942148,
-    866228071902209,
-    866228071942148,
-    866228071902209]
+    imeis = [353104320127114,
+353104320100087,
+353104320234126,
+353104320128658,
+353104320232815,
+353104320172276,
+353104320126850,
+353104320173027,
+353104320126702,
+353104320126959]
 
     for imei in imeis:
         # Using FieldFilter with properly formatted field name
-        users_ref = db.collection('productsMeizu').where(
+        users_ref = db.collection('productsAlcatel').where(
             filter=FieldFilter("`IMEI 1 `", "==", imei)
         )
         docs = users_ref.stream()
 
         for doc in docs:
-            doc_ref = db.collection('productsMeizu').document(doc.id)
+            doc_ref = db.collection('productsAlcatel').document(doc.id)
             doc_ref.update({"state": "1",
                             "buyer2": "",
                             "delidate2": ""})
             
             print(f'{doc.id} => {doc.to_dict().get("state")} => {doc.to_dict().get("buyer2")} => {doc.to_dict().get("delidate2")}')
         print("--------------------------------------------------")
+
+
+def job_setstate():
+
+    countme=0
+
+    # Using FieldFilter with properly formatted field name
+    imei_ref = db.collection('productsTCL').where(filter=FieldFilter("`buyer2`", "==", "")).where(filter=FieldFilter("`state`", "==", "2"))
+    docs = imei_ref.stream()
+
+    for doc in docs:
+        doc_ref = db.collection('productsTCL').document(doc.id)
+        doc_ref.update({"state": "1","delidate2": ""})
+        countme=countme+1      
+        print(f'{doc.id} => {doc.to_dict().get("state")} => {doc.to_dict().get("buyer2")} => {doc.to_dict().get("delidate2")}')
+        print("------------ ",countme," --------------")
+
 
 def delete_document():
 
@@ -64,5 +77,6 @@ def delete_document():
             doc_ref.delete()
             print("------------ DELETED -----------------")
 
-print("Poova DB")
-update_document()
+print("Poova DB - Job #2")
+job_setstate()
+print("Done")
