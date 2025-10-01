1e2dbb13a0bee778de661e86b5f535df5e15bb10
drlwelgama <drlwelgama@gmail.com>
2025-10-01 15:46:48 +0530

    oct1

--- poovadb.py
+++ poovadb.py
@@ -2,6 +2,7 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 from google.cloud.firestore import FieldFilter
 from google.cloud.firestore_v1.field_path import FieldPath
+import csv
 
 # Path to your service account key file
 SERVICE_ACCOUNT_KEY_PATH = "poovakey.json"
@@ -9,8 +10,9 @@ SERVICE_ACCOUNT_KEY_PATH = "poovakey.json"
 # Load credentials from the service account key file
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_KEY_PATH)
 
+
 # Initialize Firestore client with explicit credentials
-db = firestore.Client(credentials=credentials)
+db = firestore.Client(credentials=credentials,project="poovaservice")
 
 
 def update_document():
@@ -77,6 +79,45 @@ def delete_document():
             doc_ref.delete()
             print("------------ DELETED -----------------")
 
-print("Poova DB - Job #2")
-job_setstate()
+def clean_number(value, is_int=False):
+    """Remove commas and convert to float or int safely."""
+    if not value:
+        return 0 if is_int else 0.0
+    value = value.replace(",", "")
+    return int(value) if is_int else float(value)
+
+
+## ------------------ Feed CSV data ------------------------------------------------------
+def upload_csv_to_firestore(csv_file_path):
+    collection_ref = db.collection("warehouse")
+    
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        
+        # Skip header row if exists
+        next(reader, None)
+        
+        for row in reader:
+            
+            doc_data = {
+                "matID": row[0],
+                "name": row[1],
+                "model": row[2],
+                "description": row[3],
+                "receivedDate": row[4],
+                "cost": clean_number(row[5]),
+                "sellprice": clean_number(row[6]),
+                "note": row[7],
+                "qty": clean_number(row[8], is_int=True)
+            }
+            
+            # Use matID as document ID (optional), otherwise Firestore auto-generates
+            collection_ref.document(doc_data["matID"]).set(doc_data)
+
+    print("CSV upload completed successfully")
+## ----------------------------------------------------------------------------------------
+
+
+print("Poova Service DB - Job #3")
+
 print("Done")
